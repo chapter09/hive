5a2ee19ace97b9a71a1a85da5b3f0c1aa820a2ad
Raajay <raajay.v@gmail.com>
2015-09-11 16:43:48 -0600

    load jar for hve

---
+++ load_hdfs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+VERSION=apache-hive-2.0.0-SNAPSHOT-bin
+
+${HADOOP_HOME}/bin/hdfs dfs -mkdir -p /apps/${VERSION}
+${HADOOP_HOME}/bin/hdfs dfs -copyFromLocal /users/raajay86/code-netopt/hive/packaging/target/${VERSION}.tar.gz /apps/${VERSION}
+${HADOOP_HOME}/bin/hdfs dfs -copyFromLocal /users/raajay86/code-netopt/hive/packaging/target/apache-hive-2.0.0-SNAPSHOT-bin/apache-hive-2.0.0-SNAPSHOT-bin/lib/hive-exec-2.0.0-SNAPSHOT.jar /apps/${VERSION}
